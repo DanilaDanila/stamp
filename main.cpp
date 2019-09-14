e11e49ea3104f01b204cda6f6e111adca57f467d
DanilaDanila <manakov.danila@mail.ru>
2019-09-14 16:52:23 +0300

    <add> struct block

--- main.cpp
+++ main.cpp
@@ -1,5 +1,22 @@
 #include <SFML/Graphics.hpp>
 
+#define block_type           char
+#define BLOCK_NAB           (0<<0)
+#define BLOCK_TERMINATOR    (1<<0)
+#define BLOCK_PROCESS       (1<<1)
+#define BLOCK_CONDITION     (1<<2)
+#define BLOCK_IO            (1<<3)
+
+struct block
+{
+    unsigned pos_x;
+    unsigned pos_y;
+    unsigned size;
+    std::string label;
+
+    block_type type = BLOCK_NAB;
+};
+
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(1024, 576), "Stupid Algorithm Making Program");
